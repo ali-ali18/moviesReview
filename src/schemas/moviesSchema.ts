fad83b2782498da4389f73d653b1e2e7e011e3d7
ali-ali18 <alisson.camargo024@gmail.com>
2025-03-17 14:23:51 -0300

    deixando mais flexivel caso seja null

--- src/schemas/moviesSchema.ts
+++ src/schemas/moviesSchema.ts
@@ -1,11 +1,14 @@
 import { z } from "zod";
 
 export const movieSchema = z.object({
-    id: z.number().int().positive(),
-    title: z.string().min(1, "O titulo deve ter pelo menos 1 caracter"),
-    release_date: z.string().regex(/\d{4}-\d{2}-\d{2}/, "Data inválida"),
-    poster_path: z.string().min(1, "A imagem deve ter pelo menos 1 caracter").nullable(),
-    vote_average: z.number().min(0).max(10),
+	id: z.number().int().positive(),
+	title: z.string().min(1, "O titulo deve ter pelo menos 1 caracter"),
+	release_date: z.string().regex(/\d{4}-\d{2}-\d{2}/, "Data inválida"),
+	poster_path: z
+		.string()
+		.min(1, "A imagem deve ter pelo menos 1 caracter")
+		.transform((poster) => (poster === null ? "" : poster)),
+	    vote_average: z.number().min(0).max(10),
 });
 
 export const movieArraySchema = z.array(movieSchema);
